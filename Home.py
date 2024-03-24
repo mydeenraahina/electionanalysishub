d49800b2b87f73fa226a8a8434771f4b95934b82
MydeenRaahina <145376500+mydeenraahina@users.noreply.github.com>
2024-03-23 22:47:38 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,48 +1,42 @@
 import streamlit as st
 
-# Left column (Featured Visualizations)
 # Set page title and background color
 st.set_page_config(page_title="Election Data Analysis", page_icon="📊", layout="wide")
+
+# Left column (Featured Visualizations)
 with st.sidebar:
     st.success("👆Click the above Option")
 st.markdown("<h1 style='color:#ff0066'>🗳️Election Data Analysis!</h1>", unsafe_allow_html=True)
 st.markdown("---")
+
 # Main content columns
 left_column, right_column = st.columns([1, 1])
 
 with left_column:
     ## Objective:
-    
-
     st.header("📊📈Featured Visualizations")
     st.write("""
    Discover a comprehensive platform for exploring election data and gaining insights. Delve into voter turnout, party alliances, and nomination insights to uncover valuable perspectives on election trends.
     """)
-
     st.markdown("---")
     st.image("homepicture (3).png")
-    import streamlit as st
 
-
-    
 # Right column (About and Contact)
 with right_column:
     st.caption("""Our objective is to provide comprehensive analysis and insights into various aspects of elections.
                 We cover topics such as:""")
+    st.info(" - Election Nomination Insights")
+    st.info(" - Election-related metrics")
+    st.info(" - Party alliances")
+    st.info(" - Candidates Details")
+    st.info(" - Electoral Results")
+    st.info(" - AI Chat Facility")
+cols1,cols2=st.columns(2)
+# Below the main content columns
+st.title("Happy Exploring!...") 
+st.image("homepicture2.png")
 
-st.info(" - Election Nomination Insights")
-st.info(" - Election-related metrics")
-st.info(" - Party alliances")
-st.info(" - Candidates Details")
-st.info(" - Electoral Results")
-st.info(" - AI Chat Facility")
-
-with st.cols(2):
-    st.title("Happy Exploring!...") 
-    st.image("homepicture2.png")
-    
-with st.cols(1):
-    st.header("📝About")
-    st.write("""
-        This Election Data Analysis, your hub for election insights. Explore voter turnout, party alliances, Candidates Details,Elction Result and nomination trends. Meet our AI assist
-        ant for personalized assistance and deeper insights. Ask questions, get clarifications, and navigate election dynamics effortlessly!.  """)
+st.header("📝About")
+st.write("""
+    This Election Data Analysis, your hub for election insights. Explore voter turnout, party alliances, Candidates Details, Election Result and nomination trends. Meet our AI assistant for personalized assistance and deeper insights. Ask questions, get clarifications, and navigate election dynamics effortlessly!
+""")
